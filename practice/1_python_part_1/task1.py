35abf68880635d57299cdc9a8c8255cde88e7436
PRadwanski <100350850+PRadwanski@users.noreply.github.com>
2022-02-28 17:19:03 +0100

    Update task1.py

--- practice/1_python_part_1/task1.py
+++ practice/1_python_part_1/task1.py
@@ -16,3 +16,35 @@ from typing import List, Any
 
 def delete_from_list(list_to_clean: List, item_to_delete: Any) -> List:
     ...
+
+    
+    
+# Solution
+def delete_from_list(arr, redundant):
+    new_arr = []
+
+    for item in arr:
+        if item != redundant:
+            new_arr.append(item)
+    return new_arr
+
+# With Pop method
+def delete_from_list_2(arr, redundant):
+    new_list = []
+
+    while len(arr) > 0:
+        poped_item = arr.pop(0)
+        if poped_item != redundant:
+            new_list.append(poped_item)
+        else:
+            del poped_item
+        
+    return new_list
+
+
+
+my_list = [1,3,5,6,2,4,2,2,3,2]
+my_list2 = [1,3,5,6,2,4,2,2,3,2]
+
+delete_from_list(my_list, 2)
+delete_from_list_2(my_list2, 2)
