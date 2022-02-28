f77b52d71060892182733481e8745164cb0886f2
PRadwanski <100350850+PRadwanski@users.noreply.github.com>
2022-02-28 16:55:04 +0100

    Update task4.py

--- practice/1_python_part_1/task4.py
+++ practice/1_python_part_1/task4.py
@@ -11,3 +11,18 @@ from typing import List
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
     ...
+
+    
+    
+# Solution
+def calculate_power_with_difference(arr):
+
+    new_arr = []
+    for i in range(0, len(arr)):
+        if i == 0:
+            new_arr.append(arr[i]**2)
+        else:
+            new_arr.append(arr[i]**2 - (arr[i-1]**2 - arr[i-1]))
+    return new_arr
+
+calculate_power_with_difference([1,2,3,4,5,6,7])
