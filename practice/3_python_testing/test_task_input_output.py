48307e6532b2c63148b18621a93bb587653d7d43
PRadwanski <100350850+PRadwanski@users.noreply.github.com>
2022-03-09 14:08:28 +0100

    Update test_task_input_output.py

--- practice/3_python_testing/test_task_input_output.py
+++ practice/3_python_testing/test_task_input_output.py
@@ -1,22 +1,26 @@
-"""
-Write tests for a read_numbers function.
-It should check successful and failed cases
-for example:
-Test if user inputs: 1, 2, 3, 4
-Test if user inputs: 1, 2, Text
+# Exercise 3
 
-Tip: for passing custom values to the input() function
-Use unittest.mock patch function
-https://docs.python.org/3/library/unittest.mock.html#unittest.mock.patch
+def read_numbers(enters, n):
+    output = 0
+    counter = 0
+    for i in range(n-1):
+        if type(enters[i]) == int or type(enters[i]) == float:
+            output += enters[i]
+            counter += 1
 
-TIP: for testing builtin input() function create another function which return input() and mock returned value
-"""
-from unittest.mock import patch
+    if output == 0:
+        return "No numbers entered"
 
-
-def test_read_numbers_without_text_input():
-    ...
+    return f"Avg: {round(output/counter, 2)}"
 
 
-def test_read_numbers_with_text_input():
-    ...
+# tests
+data = [
+    (["Bunny", "Dog", 5, 6, 10], 5,'Avg: 5.5'),
+    (['Fasolka', "Szkic", "Duzo"], 1, "No numbers entered"),
+    ([1,2,3,4],2,'Avg: 1.0')
+]
+
+@pytest.mark.parametrize("a,b,c",data)
+def test_read_numbers(a,b,c):
+    assert read_numbers(a,b) == c
